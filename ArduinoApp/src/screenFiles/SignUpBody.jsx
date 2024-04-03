c92604042c838b1f6c207be9f33e1aa9b342a48d
Yousef Mardrosyan <104654454+azvali@users.noreply.github.com>
2024-04-03 19:42:35 -0400

    e

--- ArduinoApp/src/screenFiles/SignUpBody.jsx
+++ ArduinoApp/src/screenFiles/SignUpBody.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
+import axios from 'axios';
+
 
 
 function SignUpBody({navigation}){
